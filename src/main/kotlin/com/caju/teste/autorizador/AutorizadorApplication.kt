c3ad2c2ffc6f3a2583df89d019a65fe8d87d4482
Andre santini <andresantini123@gmail.com>
2024-10-22 16:55:12 -0300

    commit inicial teste caju

---
+++ src/main/kotlin/com/caju/teste/autorizador/AutorizadorApplication.kt
@@ -0,0 +1,11 @@
+package com.caju.teste.autorizador
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutorizadorApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutorizadorApplication>(*args)
+}
---
+++ src/test/kotlin/com/caju/teste/autorizador/AutorizadorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.caju.teste.autorizador
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutorizadorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
