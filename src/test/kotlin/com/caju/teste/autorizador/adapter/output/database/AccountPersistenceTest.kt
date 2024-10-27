2f49e32c48da8291d7e207ea2e42970b209548ee
Andre santini <andresantini123@gmail.com>
2024-10-27 13:07:04 -0300

    iniciando teste da persistencia do account

--- src/test/kotlin/com/caju/teste/autorizador/adapter/output/database/AccountPersistenceTest.kt
+++ src/test/kotlin/com/caju/teste/autorizador/adapter/output/database/AccountPersistenceTest.kt
@@ -1,10 +1,15 @@
 package com.caju.teste.autorizador.adapter.output.database
 
+import com.caju.teste.autorizador.adapter.output.database.repository.AccountRepository
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 class AccountPersistenceTest {
+
+    private val accountRepository: AccountRepository = mockk()
+    private lateinit var accountPersistence: AccountPersistence
 
     @Test
     fun findById() {
